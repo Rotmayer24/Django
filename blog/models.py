c6b1ad95762cecfc9152c09f340a0e2af5071c02
Rotmayer24 <demkovychvovan@gmail.com>
2026-01-10 00:33:24 +0100

    c

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
 # Create your models here.
+
+class AppUser(models.Model):
+    roles = [
+        ("admin", "Admin"),
+        ("user", "User"),
+    ]
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    role = models.CharField(max_length=10, choices=roles, default="user")
+    
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField(max_length=3000)
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(AppUser, on_delete=models.CASCADE, null=True, blank=True)
+    def __str__(self):
+        return self.title
+
